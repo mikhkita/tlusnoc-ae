007a3174517c103bc34af129be2165f0be8696fb
rom4es <rom4es_1993@mail.ru>
2017-11-17 17:02:56 +0700

    123

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){	
+$(document).ready(function(){
+
+    var isRetina = retina();
+
     function resize(){
        if( typeof( window.innerWidth ) == 'number' ) {
             myWidth = window.innerWidth;
@@ -12,6 +15,19 @@ $(document).ready(function(){
             myHeight = document.body.clientHeight;
         }
     }
+
+    function retina(){
+        var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
+            (min--moz-device-pixel-ratio: 1.5),\
+            (-o-min-device-pixel-ratio: 3/2),\
+            (min-resolution: 1.5dppx)";
+        if (window.devicePixelRatio > 1)
+            return true;
+        if (window.matchMedia && window.matchMedia(mediaQuery).matches)
+            return true;
+        return false;
+    }
+
     $(window).resize(resize);
     resize();
 
@@ -52,6 +68,17 @@ $(document).ready(function(){
         }
     });*/
 
+    $("img").each(function(){
+        if( $(this).attr("data-retina-src") && isRetina){
+            var img = new Image(),
+                $this = $(this);
+            img.src = $(this).attr("data-retina-src");
+            img.onload = function(){
+                $this.attr("src", $this.attr("data-retina-src"));
+            }
+        }
+    });
+
      $('.b-client-slider').slick({
         dots: false,
         arrows: true,
@@ -61,18 +88,81 @@ $(document).ready(function(){
         slidesToShow: 5,
         slidesToScroll: 1,
         speed: 600,
-        //autoplay: true,
-        //autoplaySpeed: 3000,
-        /*responsive: [
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 1100,
+              settings: {
+                slidesToShow: 4
+              }
+            },
             {
               breakpoint: 900,
               settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToShow: 3
               }
-            }
-        ]*/
+            },
+            {
+              breakpoint: 700,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 500,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+        ]
     });
+
+    var slideout = new Slideout({
+        'panel': document.getElementById('panel-page'),
+        'menu': document.getElementById('mobile-menu'),
+        'padding': 256,
+        'tolerance': 70
+    });
+
+    $('.burger-menu').click(function() {
+        slideout.open();
+        $(".b-menu-overlay").show();
+        return false;
+    });
+    $('.b-menu-overlay').click(function() {
+        slideout.close();
+        $('.b-menu-overlay').hide();
+        return false;
+    });
+
+    slideout.on('open', function() {
+        $('.burger-menu').addClass("menu-on");
+        $(".b-menu-overlay").show();
+    });
+
+    slideout.on('close', function() {
+        $('.burger-menu').removeClass("menu-on");
+        setTimeout(function(){
+            $("body").unbind("touchmove");
+            $(".b-menu-overlay").hide();
+        },100);
+    });
+
+    /*bindCloseMenu("menu");
+    bindCloseMenu("b-menu-overlay");
+
+    function bindCloseMenu(id){
+        var swipeh = new MobiSwipe(id);
+        swipeh.direction = swipeh.HORIZONTAL;
+        swipeh.onswipeleft = function() { 
+            $("body").bind("touchmove", function(e){
+                e.preventDefault();
+                return false;
+            });
+        slideout.close(); 
+        };
+    }*/
     
 	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
  //    var myOptions = {
